6659791b67fcabc14103b59f4879199a80dbf99d
DMacMakes <56147357+DMacMakes@users.noreply.github.com>
2025-12-14 21:04:07 +1100

    One ui image object, in need of friends.

---
+++ Assets/TimeBar.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+public class TimeBar : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("TimeBar reporting.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
