b7f5b03f9466771f9490413f9516633e216099c0
DMacMakes <56147357+DMacMakes@users.noreply.github.com>
2025-12-16 09:39:14 +1100

    Mouse hover on end draggers turns cursor into east-west arrows

--- Assets/TB_DragHandle_Asym.cs
+++ Assets/TB_DragHandle_Asym.cs
@@ -1,11 +1,30 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragHandle_Asym : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class DragHandle_Asym : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerEnterHandler, IPointerExitHandler
 {
     public TimeBar_Grabby timeBar;
     public string whichEnd;
+    public Texture2D cursorEastWest;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
 
+    void Start()
+    {
+        //hotSpot = new Vector2(25, 0);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //Debug.Log("Pointer entered..");
+        Cursor.SetCursor(cursorEastWest, hotSpot, cursorMode);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        // Pass 'null' to the texture parameter to use the default system cursor.
+        Cursor.SetCursor(null, Vector2.zero, cursorMode);
+    }
     public void OnBeginDrag(PointerEventData eventData)
     {
         // Called when drag starts
